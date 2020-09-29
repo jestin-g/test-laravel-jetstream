de035f25abf0347d7af8fcc3cb706bfee90307be
jestin-g <jestingabriel@gmail.com>
2020-09-29 11:02:28 +0200

    change 'description' field from string to text

--- database/migrations/2020_09_29_064154_create_courses_table.php
+++ database/migrations/2020_09_29_064154_create_courses_table.php
@@ -16,7 +16,7 @@ class CreateCoursesTable extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
--- database/migrations/2020_09_29_083209_create_episodes_table.php
+++ database/migrations/2020_09_29_083209_create_episodes_table.php
@@ -16,7 +16,7 @@ class CreateEpisodesTable extends Migration
         Schema::create('episodes', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->string('video_url');
             $table->foreignId('course_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
