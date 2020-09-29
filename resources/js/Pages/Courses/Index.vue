4d7b803a68740820fd1107cde3673c70e8c0d842
jestin-g <jestingabriel@gmail.com>
2020-09-29 14:23:56 +0200

    add episodes_count and user

--- resources/js/Pages/Courses/Index.vue
+++ resources/js/Pages/Courses/Index.vue
@@ -5,9 +5,10 @@
         </template>
         <div class="py-3" v-for="course in this.courseList" v-bind:key="course.id">
             <div class="mx-8 bg-white rounded shadow p-4">
+                <div class="text-sm text-gray-500">Mise en ligne par {{ course.user.name }}</div>
                 <div class="flex justify-between items-center">
                     <div class="text-3xl">{{ course.title }}</div>
-                    <div class="text-gray-400">25 épisodes</div>
+                    <div class="text-gray-400">{{ course.episodes_count }} épisodes</div>
                 </div>  
                 <div class="text-sm text-gray-500">{{ course.description }}</div>
                 <a href="#" class="bg-indigo-500 text-white rounded px-2 py-1 mt-3 inline-block hover:bg-indigo-700">Voir la formation</a>
